c13f832c0f54d984d752a30ae4308603bcb1aaec
rachelc313 <111570965+rachelc313@users.noreply.github.com>
2022-12-08 15:27:30 -0600

    updating rstudio script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,12 @@ Suspension_Coil <- read.csv("Suspension_Coil.csv",check.names = F,stringsAsFacto
 
 # Write an RScript that creates a total_summary dataframe using the summarize() function to
 # get the mean, median, and standard deviation of the suspension coil's PSI column.
+total_summary <- Suspension_Coil %>%
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize
+# functions to group each manufacturing lot by the mean, median, variance, and standard deviation
+# of the suspension coil's PSI column
+lot_summary <- Suspension_Coil %>%
+  group_by(Manufacturing_Lot) %>%
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
